26f6d094592f8310ff5ffaa4ea5f94d53b2b5b12
efren <efrent84@hotmail.com>
2024-12-12 02:27:08 -0500

    assig1

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -5,10 +5,14 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
-
+SELECT * FROM customer
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
+
+SELECT * FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10
 
 
 
@@ -16,9 +20,13 @@ sorted by customer_last_name, then customer_first_ name. */
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
 
+SELECT * FROM customer_purchases
+WHERE product_id = 4
+OR product_id = 9;
 
 -- option 2
-
+SELECT * FROM customer_purchases
+WHERE product_id IN (4,9);
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
